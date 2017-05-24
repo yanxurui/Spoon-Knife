6a82ff1993b0226e7a4676355f3e21868b70632a
yanxurui <617080352@qq.com>
2017-05-24 14:43:39 +0800

    start memcached mannully

--- test_module1.py
+++ test_module1.py
@@ -3,13 +3,13 @@ from pymemcache.client.base import Client
 
 class TestMemcached(unittest.TestCase):
     def test_set_get(self):
-        client = Client(('localhost', 11211))
+        client = Client(('localhost', 11212))
         client.set('some_key', 'some_value')
         result = client.get('some_key')
         self.assertEqual(result, 'some_value')
 
     def test_delete(self):
-        client = Client(('localhost', 11211))
+        client = Client(('localhost', 11212))
         client.set('some_key', 'some_value')
         client.delete('some_key')
         result = client.get('some_key')
