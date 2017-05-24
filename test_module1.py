b171b77051b9ae40c572dba65557289c4c3d05f6
yanxurui <617080352@qq.com>
2017-05-24 10:30:13 +0800

    test using travis-ci

---
+++ test_module1.py
@@ -0,0 +1,15 @@
+import unittest
+from pymemcache.client.base import Client
+
+class TestMemcached(unittest.TestCase):
+    def test_set_get(self):
+        client = Client(('localhost', 11211))
+        client.set('some_key', 'some_value')
+        result = client.get('some_key')
+        self.assertEqual(result, 'some_value')
+
+    def test_delete(self):
+        client = Client(('localhost', 11211))
+        client.delete('some_key')
+        result = client.get('some_key')
+        self.assertEqual(result, None)
