20e9d3ad71beb987553c8a8a4e8e76f7cc29ef24
yanxurui <617080352@qq.com>
2017-05-25 10:24:49 +0800

    use 2 memcached

--- test_module1.py
+++ test_module1.py
@@ -3,7 +3,7 @@ from pymemcache.client.base import Client
 
 class TestMemcached(unittest.TestCase):
     def test_set_get(self):
-        client = Client(('localhost', 11212))
+        client = Client(('localhost', 11211))
         client.set('some_key', 'some_value')
         result = client.get('some_key')
         self.assertEqual(result, 'some_value')
